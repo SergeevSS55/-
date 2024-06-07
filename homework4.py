1b988238fa614a0f8217f1a4a2cd8436c4b96f03
SergeevSS55 <yourhonor55@mail.ru>
2024-06-07 18:37:13 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,6 @@
+my_string = input('Как Вас зовут? ')
+print("Ваше имя содержит ", len(my_string), "символов")
+my_string = ('Планируете изучить новое?')
+print(my_string.upper(), my_string.lower(),
+      my_string.replace(' ', ''),
+      my_string[0], my_string[-1])
